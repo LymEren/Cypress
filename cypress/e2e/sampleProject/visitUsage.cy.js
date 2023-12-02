dc8857f995d6c8ceb9a3137f0257f33ffd6ef314
Eyyub Eren <eyyub.erenn@gmail.com>
2023-12-02 16:49:18 +0300

    Testcase updated

--- cypress/e2e/sampleProject/visitUsage.cy.js
+++ cypress/e2e/sampleProject/visitUsage.cy.js
@@ -1,10 +1,24 @@
 
-describe('Main Commands', () => {
-    it('Browse website', () => {
+describe('General test automation scenario', () => {
+    beforeEach(() => {
         cy.visit('https://automationexercise.com/')
-        cy.xpath("//a[text() =' Signup / Login']").click()
-        cy.xpath("(//input[@name ='email'])[1]").type('qakyhu@imagepoet.net')
-        cy.xpath("(//input[@name ='password'])[1]").type('Abc1234!')
-        cy.xpath("//button[text() ='Login']").click()
+      });
+    it('Login', () => {
+        cy.xpath('//a[text() =" Signup / Login"]').click()
+        cy.xpath('(//input[@name ="email"])[1]').type('qakyhu@imagepoet.net')
+        cy.xpath('(//input[@name ="password"])[1]').type('Abc1234!')
+        cy.xpath('//button[text() ="Login"]').click()
+
+        cy.log('Checking offer price.')
+        cy.xpath('(//h2[following-sibling::p[contains(text(), "Blue Top")]])[1]')
+        .should('be.visible').invoke('text')
+        .then(text => {
+            if (text== 'Rs. 500'){
+                cy.log('Text content is correct.')
+            }
+            else {
+                cy.log('Text content is incorrect.')
+            }
+        })
     })
 });
