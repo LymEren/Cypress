57b0dc9ee31c9a15ab36b7464b27139cdf267b13
Eyyub Eren <eyyub.erenn@gmail.com>
2023-12-04 01:28:43 +0300

    Testcase updated

--- cypress/e2e/sampleProject/locatorMainPage.cy.js
+++ cypress/e2e/sampleProject/locatorMainPage.cy.js
@@ -11,9 +11,17 @@ const mainPage = {
     // Items Page
     textBlueTopPrice: '(//h2[following-sibling::p[contains(text(), "Blue Top")]])[1]',
     buttonBlueTopAddBasket: '(//h2[following-sibling::p[contains(text(), "Blue Top")]])[1]/following-sibling::a',
+    textAllProducts: '//h2[text()="All Products"]',
     
     textAdded: '//*[text()="Added!"]',
-    buttonContinueShopping: '//*[text()="Continue Shopping"]'
+    buttonContinueShopping: '//*[text()="Continue Shopping"]',
+    buttonShoppingCart: '//a[text()=" Cart"]',
+
+    // Shoppin Cart
+    textBlueTop: '//a[text()="Blue Top"]',
+    buttonDeleteItem: '//a[@class="cart_quantity_delete"]',
+    textShoppingCardEmpty: '//b[text()="Cart is empty!"]',
+    buttonClickHere: '//u[text()="here"]'
 };
   
   export default mainPage;
--- cypress/e2e/sampleProject/mainPage.cy.js
+++ cypress/e2e/sampleProject/mainPage.cy.js
@@ -9,21 +9,31 @@ describe('General test automation scenario', () => {
         cy.xpath(mainPage.textLogin).click()
         cy.xpath(mainPage.inputEmail).type('qakyhu@imagepoet.net')
         cy.xpath(mainPage.inputPassword).type('Abc1234!')
-        cy.xpath().click()
+        cy.xpath(mainPage.buttonLogin).click()
 
         cy.log('Checking offer price.')
-        cy.xpath(mainPage.blueTopPrice)
+        cy.xpath(mainPage.textBlueTopPrice)
         .should('be.visible').invoke('text')
         .then(text => {
             if (text== 'Rs. 500'){
                 cy.log('Text content is correct.')
                 cy.xpath(mainPage.buttonBlueTopAddBasket).click()
-                cy.xpath().should('be.visible')
-                cy.xpath().click()
+                cy.xpath(mainPage.textAdded).should('be.visible')
+                cy.xpath(mainPage.buttonContinueShopping).click()
+                cy.scrollTo('top')
+                cy.xpath(mainPage.buttonShoppingCart).click()
+                
+                // Check shopping cart and delete existing item
+                cy.xpath(mainPage.textBlueTop).should('be.visible')
+                cy.xpath(mainPage.buttonDeleteItem).click()
+                cy.xpath(mainPage.textShoppingCardEmpty).should('be.visible')
+                cy.xpath(mainPage.buttonClickHere).click()
+                cy.xpath(mainPage.textAllProducts).should('be.visible')
             }
             else {
                 cy.log('Text content is incorrect. Please check related offer')
             }
+        
         })
     })
 });
